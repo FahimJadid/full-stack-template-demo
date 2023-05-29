eb40be377a2285ddca5472d4ec692eea2c3978b0
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-30 04:28:19 +0600

    added variables & MongoConnection

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+
+let db,
+  dbConnectionString = process.env.DB_STRING,
+  dbName = '',
+  collection;
+
+MongoClient.connect(dbConnectionString).then((client) => {
+  console.log('Database connection Established');
+  db = client.db(dbName);
+  collection = db.collection('');
+});
